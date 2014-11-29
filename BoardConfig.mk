6229264e9a513c2f18be77270d8ef834e1a4a1e3
Y300-0100 <bosko.brajovich@gmail.com>
2014-11-29 21:25:11 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,7 +18,16 @@
 -include vendor/huawei/u8833/BoardConfigVendor.mk
 
 # Architecture and CPU
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
+TARGET_CPU_VARIANT := cortex-a5
+TARGET_BOARD_PLATFORM := msm7x27a
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_ARCH_LOWMEM := true
 
 TARGET_BOOTLOADER_BOARD_NAME := u8833
 TARGET_OTA_ASSERT_DEVICE := u8833,hwu8833,u8951,hwu8951
